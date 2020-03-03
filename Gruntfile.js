663ab1f3f7430dce4d43be4f01f58699042350ae
jilinlin <1695729490@qq.com>
2020-03-03 15:01:34 +0800

    htmlmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,18 @@
 module.exports = function(grunt){
     grunt.initConfig({
-        run:{
-            api:{
-                options:{wait:false},
-                args:['./app.js']
-            }
-        },
-        mochacli:{
-            all:['test/*.js'],
+        htmlmin:{
             options:{
-                reportor:'spec',
-                bail:true
+                removeComments:true,
+                collaspWhitespace:true
+            },
+            files:{
+                src:'./index.html',
+                dest:'dist/index.html'
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-mocha-cli');
-    grunt.loadNpmTasks('grunt-run');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-    grunt.registerTask('default',['run','mochacli','stop:api']);
+    grunt.registerTask('default',['htmlmin']);
 };
